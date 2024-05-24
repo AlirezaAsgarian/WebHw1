4999c2d5a9adb0e7486e6a7741393ea4918f2a50
am <a.mjahromy1648@gmail.com>
2024-05-24 11:10:26 +0330

    Some refactor + improve caching

--- src/main/java/ir/aa/weatherprediction/rest/CountryController.java
+++ src/main/java/ir/aa/weatherprediction/rest/CountryController.java
@@ -1,135 +1,46 @@
 package ir.aa.weatherprediction.rest;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.Caching;
 import org.springframework.data.repository.query.Param;
-import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestClient;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 public class CountryController {
 
-    private static final int pageSize = 10;
-
-    private static final String countriesApiUri = "https://countriesnow.space/api/v0.1/countries";
-    private static final String apiNinjasUri = "https://api.api-ninjas.com/v1";
-
-    private static final String ninjasApiKey = "0yvb2E7SNK40wA0iseRj6Q==D0qe6dl6L9zszLh3";
-
-    private static final RestClient restClient = RestClient.create();
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+    private CountryService service;
 
     @Cacheable("countries")
     @GetMapping("/countries/all")
     ObjectNode getAllCountries() {
-        List<String> countriesList = fetchAllCountriesList();
-        return countryListToJson(countriesList);
+        return service.getAllCountries();
     }
 
     @GetMapping("/countries")
     ObjectNode getCountriesPage(@Param("page") Integer page, HttpServletRequest request) {
-        if (page != null && page <= 0) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid page number %s".formatted(page));
-        }
-        JsonNode jsonResponse = restClient.get()
-                .uri("http://%s:%s/countries/all".formatted(request.getServerName(), request.getServerPort()))
-                .retrieve().body(JsonNode.class);
-        if (page == null) {
-            return (ObjectNode) jsonResponse;
-        }
-        List<String> allCountriesList = new ArrayList<>();
-        jsonResponse.get("countries").forEach(country -> allCountriesList.add(country.get("name").textValue()));
-        int pageCount = (allCountriesList.size() + pageSize - 1) / pageSize;
-        if (page > pageCount) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "This list has %s pages".formatted(pageCount));
-        }
-        List<String> countriesList = allCountriesList.subList((page - 1) * pageSize, Integer.min(page * pageSize, allCountriesList.size()));
-        ObjectNode rootNode = countryListToJson(countriesList);
-        String commonUri = "http://%s:%s".formatted(request.getServerName(), request.getServerPort()) + "/countries?page=%s";
-        rootNode.put("allCount", allCountriesList.size());
-        rootNode.put("lastPage", pageCount);
-        ObjectNode linksNode = rootNode.putObject("_links");
-        linksNode.putObject("self").put("href", commonUri.formatted(page));
-        if (page > 1) {
-            linksNode.putObject("prev").put("href", commonUri.formatted(page - 1));
-        }
-        if (page < pageCount) {
-            linksNode.putObject("next").put("href", commonUri.formatted(page + 1));
-        }
-        return rootNode;
-    }
-
-    List<String> fetchAllCountriesList() {
-        JsonNode jsonResponse = restClient.get()
-                .uri(countriesApiUri)
-                .retrieve().body(JsonNode.class);
-        List<String> countries = new ArrayList<>();
-        jsonResponse.get("data").forEach(country -> countries.add(country.get("country").textValue()));
-        return countries;
-    }
-
-    ObjectNode countryListToJson(List<String> countryList) {
-        ObjectNode rootNode = objectMapper.createObjectNode();
-        ArrayNode countriesNode = rootNode.putArray("countries");
-        for (String country : countryList) {
-            countriesNode.addObject().put("name", country);
-        }
-        rootNode.put("count", countryList.size());
-        return rootNode;
+        String serverSelfUri = "http://%s:%s".formatted(request.getServerName(), request.getServerPort());
+        return service.getCountriesPage(page, serverSelfUri);
     }
 
     @Cacheable("country")
     @GetMapping("/countries/{name}")
     ObjectNode getCountryInfo(@PathVariable("name") String countryName) {
-        JsonNode jsonResponse = restClient.get()
-                .uri("%s/country?name=%s".formatted(apiNinjasUri, countryName))
-                .header("X-Api-Key", ninjasApiKey)
-                .retrieve().body(JsonNode.class);
-        if (jsonResponse.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No such country name");
-        }
-        JsonNode countryInfo = jsonResponse.get(0);
-        ObjectNode resultObjectNode = objectMapper.createObjectNode();
-        List.of("name", "capital", "iso2", "population", "pop_growth", "currency")
-                .forEach(x -> resultObjectNode.put(x, countryInfo.get(x)));
-        return resultObjectNode;
+        return service.getCountryInfo(countryName);
     }
 
     @Cacheable("weather")
     @GetMapping("/countries/{name}/weather")
-    ObjectNode getCapitalWeatherInfo(@PathVariable("name") String countryName) {
-        JsonNode jsonResponseOfCountry = restClient.get()
-                .uri("%s/country?name=%s".formatted(apiNinjasUri, countryName))
-                .header("X-Api-Key", ninjasApiKey)
-                .retrieve().body(JsonNode.class);
-        if (jsonResponseOfCountry.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No such country name");
-        }
-        String capital = jsonResponseOfCountry.get(0).get("capital").textValue();
-        JsonNode jsonResponseOfWeather = restClient.get()
-                .uri("%s/weather?city=%s".formatted(apiNinjasUri, capital))
-                .header("X-Api-Key", ninjasApiKey)
-                .retrieve().body(JsonNode.class);
-        ObjectNode resultObjectNode = objectMapper.createObjectNode();
-        resultObjectNode.put("country_name", countryName);
-        resultObjectNode.put("capital", capital);
-        List.of("wind_speed", "wind_degrees", "temp", "humidity")
-                .forEach(x -> resultObjectNode.put(x, jsonResponseOfWeather.get(x)));
-        return resultObjectNode;
+    ObjectNode getCapitalWeatherInfo(@PathVariable("name") String countryName, HttpServletRequest request) {
+        String serverSelfUri = "http://%s:%s".formatted(request.getServerName(), request.getServerPort());
+        return service.getCapitalWeatherInfo(countryName, serverSelfUri);
     }
 
     @Caching(evict = {
--- src/test/java/ir/aa/weatherprediction/rest/CountriesTest.java
+++ src/test/java/ir/aa/weatherprediction/rest/CountriesTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.classic.methods.HttpUriRequest;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
@@ -11,9 +12,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CountriesTest {
@@ -22,12 +28,13 @@ public class CountriesTest {
     private int port;
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final CloseableHttpClient httpClient = HttpClientBuilder.create().build();
 
     @Test
     void testCountriesAll() throws Exception {
         HttpUriRequest request = new HttpGet("http://localhost:%s/countries".formatted(port));
-        CloseableHttpResponse response = HttpClientBuilder.create().build().execute(request);
-        Assertions.assertEquals(response.getCode(), 200);
+        CloseableHttpResponse response = httpClient.execute(request);
+        Assertions.assertEquals(response.getCode(), HttpStatus.OK.value());
         String responseString = EntityUtils.toString(response.getEntity());
         JsonNode jsonNode = objectMapper.readTree(responseString);
         Assertions.assertEquals(
@@ -44,8 +51,8 @@ public class CountriesTest {
     @Test
     void testCountriesFirstPage() throws Exception {
         HttpUriRequest request = new HttpGet("http://localhost:%s/countries?page=1".formatted(port));
-        CloseableHttpResponse response = HttpClientBuilder.create().build().execute(request);
-        Assertions.assertEquals(response.getCode(), 200);
+        CloseableHttpResponse response = httpClient.execute(request);
+        Assertions.assertEquals(response.getCode(), HttpStatus.OK.value());
         String responseString = EntityUtils.toString(response.getEntity());
         JsonNode jsonNode = objectMapper.readTree(responseString);
         Assertions.assertEquals(
@@ -54,6 +61,59 @@ public class CountriesTest {
         Assertions.assertEquals(
                 Set.of("self", "next"),
                 getFieldNamesOfNode(jsonNode.get("_links")));
+    }
+
+    @Test
+    void testCorrectCountry() throws Exception {
+        HttpUriRequest request = new HttpGet("http://localhost:%s/countries/Iran".formatted(port));
+        CloseableHttpResponse response = httpClient.execute(request);
+        Assertions.assertEquals(response.getCode(), HttpStatus.OK.value());
+        String responseString = EntityUtils.toString(response.getEntity());
+        JsonNode jsonNode = objectMapper.readTree(responseString);
+        Assertions.assertEquals(
+                Set.of("name", "capital", "iso2", "population", "pop_growth", "currency"),
+                getFieldNamesOfNode(jsonNode));
+        Assertions.assertEquals(
+                Set.of("name", "code"),
+                getFieldNamesOfNode(jsonNode.get("currency")));
+        Assertions.assertEquals(
+                "Iran, Islamic Republic Of",
+                jsonNode.get("name").asText());
+        Assertions.assertEquals(
+                "Tehran",
+                jsonNode.get("capital").asText());
+    }
+
+    @Test
+    void testIncorrectCountryNotFound() throws Exception {
+        HttpUriRequest request = new HttpGet("http://localhost:%s/countries/Fake".formatted(port));
+        CloseableHttpResponse response = httpClient.execute(request);
+        Assertions.assertEquals(response.getCode(), HttpStatus.NOT_FOUND.value());
+    }
+
+    @Test
+    void testCorrectWeather() throws Exception {
+        HttpUriRequest request = new HttpGet("http://localhost:%s/countries/Iran/weather".formatted(port));
+        CloseableHttpResponse response = httpClient.execute(request);
+        Assertions.assertEquals(response.getCode(), HttpStatus.OK.value());
+        String responseString = EntityUtils.toString(response.getEntity());
+        JsonNode jsonNode = objectMapper.readTree(responseString);
+        Assertions.assertEquals(
+                Set.of("country_name", "capital", "temp", "wind_speed", "wind_degrees", "humidity"),
+                getFieldNamesOfNode(jsonNode));
+        Assertions.assertEquals(
+                "Iran",
+                jsonNode.get("country_name").asText());
+        Assertions.assertEquals(
+                "Tehran",
+                jsonNode.get("capital").asText());
+    }
+
+    @Test
+    void testIncorrectWeatherNotFound() throws Exception {
+        HttpUriRequest request = new HttpGet("http://localhost:%s/countries/Fake/weather".formatted(port));
+        CloseableHttpResponse response = httpClient.execute(request);
+        Assertions.assertEquals(response.getCode(), HttpStatus.NOT_FOUND.value());
     }
 
     private Set<String> getFieldNamesOfNode(JsonNode node) {
